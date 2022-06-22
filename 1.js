ca4fb0dd6f14b36feb731ed7830602dd6d6b9b9f
nowkoai <hyamasit@gmail.com>
2022-06-22 14:45:19 +0900

    123

--- 1.js
+++
@@ -1,9 +0,0 @@
-var $countA = 0;
-function countUpA() {
-    document.getElementById( "sub" ).innerHTML = ++$countA;
-}
-
-var $countB = 0;
-function countUpB() {
-    document.getElementById( "good" ).innerHTML = ++$countB;
-}
