1ab13566223aff6105a9cc9a7fcb4423baf6486b
nowkoai <hyamasit@gmail.com>
2022-06-22 16:44:12 +0900

    last

--- count.js
+++ count.js
@@ -1,37 +1,30 @@
 var $countA = 0;
+// var $countB = 0;
 
 function countUpA() {
+  // カウンター
   console.log("countUpA")
-  document.getElementById( "goodcount" ).innerHTML = ++$countA;
+  document.getElementById("goodcount").innerHTML = ++$countA;
 
-
+  // アニメーション
   document.getElementById('like').animate({
     // background: ["#fcc", "#f66"],
-    background: ['rgb(255, 255, 255)', 'rgb(226, 226, 226)'],
+    // background: ['rgb(255, 255, 255)', 'rgb(226, 226, 226)'],
+    background: ['#fff', '#F2F2F2'],
     boxShadow: '0 0 2px rgb(100, 100 ,100) inset'
-  }, 1000 );
+  }, 500 );
+}
 
+function countUpB() {
+  // カウンター
+  // console.log("countUpB")
+  // document.getElementById("badcount").innerHTML = ++$countB;
 
-  // ////////////////
-  // // document.getElementById('like').style.transition = 'all 0.4s ease-in-out';
-  // document.getElementById('like').style.backgroundColor = 'rgb(226, 226, 226)';
-  // document.getElementById('like').style.boxShadow = '0 0 2px rgb(100, 100 ,100) inset';
-  //
-  // // アニメーション終了後、styleを削除
-  // setTimeout(function() {
-  //   // document.getElementById('like').style.backgroundColor = 'rgb(255, 255, 255)';
-  //   // document.getElementById('like').style.boxShadow = '0 0 0';
-  //
-  //   // document.getElementById('like').animate(
-  //   // 	[
-  //   // 		{ backgroundColor: '#4169e1' },
-  //   // 		{ backgroundColor: '#66ccff' }
-  //   // 	],
-  //   // 	{
-  //   // 		duration: 1000,
-  //   // 		// iterations: Infinity,
-  //   // 		// direction: 'alternate'
-  //   // 	}
-  //   // )
-  // }, 1000);
+  // アニメーション
+  document.getElementById('dislike').animate({
+    // background: ["#fcc", "#f66"],
+    // background: ['rgb(255, 255, 255)', 'rgb(226, 226, 226)'],
+    background: ['#fff', '#F2F2F2'],
+    boxShadow: '0 0 2px rgb(100, 100 ,100) inset'
+  }, 500 );
 }
